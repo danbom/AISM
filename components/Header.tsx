a1f1abdc0af2a6fb4809fda1c0a736dff62f9948
mineunyoung <bear0369@naver.com>
2022-05-12 16:24:39 +0900

    Feat: "헤더 틀 완성"

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,9 +1,28 @@
+import { useState } from "react";
 import Link from "next/link";
 import { Grid, Text, Container, Tooltip } from "@nextui-org/react";
+import {
+  Category,
+  Work,
+  People,
+  Paper,
+  InfoSquare,
+  Message,
+  CloseSquare,
+} from "react-iconly";
 import styled from "styled-components";
-import { ReactNode } from "react";
 
 const Header = () => {
+  const [expended, setExpended] = useState(false);
+
+  const Logo = (
+    <Link href="/">
+      <StyledText size={15} weight="medium">
+        Logo
+      </StyledText>
+    </Link>
+  );
+
   const Menu = (title: string) => (
     <Grid>
       <Link href={`/${title}`}>
@@ -19,10 +38,10 @@ const Header = () => {
   const MenuWithSub = (title: string, subMenu: JSX.Element) => (
     <Grid>
       <Tooltip
+        content={subMenu}
         placement="bottom"
         trigger="hover"
         hideArrow
-        content={subMenu}
         css={{ zIndex: 9999 }}
       >
         <Link href={`/${title}`}>
@@ -52,11 +71,29 @@ const Header = () => {
     </Grid.Container>
   );
 
+  const ExpendedMenu = (icon: JSX.Element, title: string) => (
+    <Grid>
+      <Link href={`/${title}`}>
+        <ExpendedMenuBtn>
+          {icon}
+          <StyledText css={{ paddingLeft: "0.6rem" }} size={15} weight="medium">
+            {title}
+          </StyledText>
+        </ExpendedMenuBtn>
+      </Link>
+    </Grid>
+  );
+
+  const toggleExpended = () => {
+    setExpended(!expended);
+  };
+
   return (
     <StyledHeader>
-      <StyledGrid justify="center">
-        <StyledContainer>
+      <BigGrid justify="center">
+        <Container display="flex">
           <Grid xs alignItems="center">
+            {Logo}
             {/* <StyledImg src="/static/image/AISM_logo_nodesc.png" alt="Logo" /> */}
           </Grid>
           <Grid xs={7} justify="center">
@@ -78,8 +115,53 @@ const Header = () => {
             </Grid.Container>
           </Grid>
           <Grid xs />
-        </StyledContainer>
-      </StyledGrid>
+        </Container>
+      </BigGrid>
+      <SmallGrid justify="center">
+        <Container display="flex">
+          <Grid xs />
+          <Grid xs={7} justify="center" alignItems="center">
+            {Logo}
+          </Grid>
+          <Grid xs justify="flex-end">
+            <MenuBtn onClick={toggleExpended}>
+              {expended ? (
+                <CloseSquare set="light" primaryColor="#212121" />
+              ) : (
+                <Category set="light" primaryColor="#212121" />
+              )}
+            </MenuBtn>
+          </Grid>
+        </Container>
+      </SmallGrid>
+      {expended && (
+        <ExpendedGrid>
+          <Container>
+            <Grid.Container direction="column" gap={1}>
+              {ExpendedMenu(
+                <Work set="light" primaryColor="#212121" />,
+                "introduce"
+              )}
+              {ExpendedMenu(
+                <Paper set="light" primaryColor="#212121" />,
+                "program"
+              )}
+              {ExpendedMenu(
+                <People set="light" primaryColor="#212121" />,
+                "instructor"
+              )}
+              {ExpendedMenu(
+                <InfoSquare set="light" primaryColor="#212121" />,
+                "notice"
+              )}
+              {ExpendedMenu(
+                <Message set="light" primaryColor="#212121" />,
+                "faq"
+              )}
+            </Grid.Container>
+          </Container>
+        </ExpendedGrid>
+      )}
     </StyledHeader>
   );
 };
@@ -94,10 +176,72 @@ const StyledHeader = styled.div`
   z-index: 100;
 `;
 
+const StyledGrid = styled(Grid)`
+  background: hsla(0, 0%, 100%, 0.8);
+  display: flex;
+  align-items: center;
+  width: 100%;
+  height: 100%;
+
+  @media screen and (min-width: 960px) {
+    backdrop-filter: saturate(180%) blur(10px);
+    box-shadow: rgb(2 1 1 / 10%) 0px 5px 20px -5px;
+  }
+`;
+
+const BigGrid = styled(StyledGrid)`
+  @media screen and (max-width: 960px) {
+    display: none;
+  }
+`;
+
+const SmallGrid = styled(StyledGrid)`
+  @media screen and (min-width: 960px) {
+    display: none;
+  }
+`;
+
+const ExpendedGrid = styled(Grid)`
+  background: hsla(0, 0%, 100%, 0.8);
+  display: flex;
+  align-items: center;
+  width: 100%;
+  padding: 0.5rem 0;
+
+  @media screen and (min-width: 960px) {
+    display: none;
+  }
+`;
+
 const MenuBtn = styled.button`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   background: transparent;
   padding: 0.6rem 1rem;
   min-width: min-content;
+  border-radius: 14px;
+  outline: none;
+  border: none;
+  transition: 0.2s ease;
+  cursor: pointer;
+
+  &::first-letter {
+    text-transform: uppercase;
+  }
+
+  &:hover {
+    background-color: rgba(2, 32, 71, 0.05);
+  }
+`;
+
+const ExpendedMenuBtn = styled(MenuBtn)`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  background: transparent;
+  padding: 1rem;
+  min-width: 100%;
   border-radius: 14px;
   outline: none;
   border: none;
@@ -112,26 +256,7 @@ const MenuBtn = styled.button`
   }
 `;
 
-const StyledGrid = styled(Grid)`
-  backdrop-filter: saturate(180%) blur(10px);
-  box-shadow: rgb(2 1 1 / 10%) 0px 5px 20px -5px;
-  background: hsla(0, 0%, 100%, 0.8);
-  display: flex;
-  align-items: center;
-  width: 100%;
-  height: 100%;
-`;
-
-const StyledContainer = styled(Container)`
-  display: flex;
-`;
-
-const StyledImg = styled.img`
-  width: 5rem;
-`;
-
 const StyledText = styled(Text)`
-  opacity: 0.8;
   font-weight: 500;
 
   &::first-letter {
