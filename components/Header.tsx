af568c77e21c1c6758660bb6698925ca3576fd0b
mineunyoung <bear0369@naver.com>
2022-05-12 11:52:11 +0900

    Feat: "헤더 메뉴 라우팅 및 스타일링"

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,122 +1,80 @@
 import Link from "next/link";
-import {
-  Grid,
-  Card,
-  Text,
-  Button,
-  Container,
-  Tooltip,
-} from "@nextui-org/react";
+import { Grid, Text, Container, Tooltip } from "@nextui-org/react";
 import styled from "styled-components";
+import { ReactNode } from "react";
 
 const Header = () => {
+  const Menu = (title: string) => (
+    <Grid>
+      <Link href={`/${title}`}>
+        <MenuBtn>
+          <StyledText size={15} weight="medium">
+            {title}
+          </StyledText>
+        </MenuBtn>
+      </Link>
+    </Grid>
+  );
+
+  const MenuWithSub = (title: string, subMenu: JSX.Element) => (
+    <Grid>
+      <Tooltip
+        placement="bottom"
+        trigger="hover"
+        hideArrow
+        content={subMenu}
+        css={{ zIndex: 9999 }}
+      >
+        <Link href={`/${title}`}>
+          <MenuBtn>
+            <StyledText size={15} weight="medium">
+              {title}
+            </StyledText>
+          </MenuBtn>
+        </Link>
+      </Tooltip>
+    </Grid>
+  );
+
+  const SubMenu = (menu: string, arr: Array<string>) => (
+    <Grid.Container gap={1}>
+      {arr.map((title, index) => (
+        <Grid key={index}>
+          <Link href={`/${menu}/${title}`}>
+            <MenuBtn>
+              <StyledText size={13} weight="medium">
+                {title}
+              </StyledText>
+            </MenuBtn>
+          </Link>
+        </Grid>
+      ))}
+    </Grid.Container>
+  );
+
   return (
     <StyledHeader>
       <StyledGrid justify="center">
         <StyledContainer>
           <Grid xs alignItems="center">
-            <StyledImg src="/static/image/AISM_logo_nodesc.png" alt="Logo" />
+            {/* <StyledImg src="/static/image/AISM_logo_nodesc.png" alt="Logo" /> */}
           </Grid>
           <Grid xs={7} justify="center">
             <Grid.Container gap={2} justify="center">
-              <Grid>
-                <Tooltip
-                  placement="bottom"
-                  trigger="hover"
-                  hideArrow
-                  content={
-                    <Grid.Container gap={1}>
-                      <Grid>
-                        <StyledButton light color="primary" auto>
-                          <StyledText size={13} weight="medium">
-                            History
-                          </StyledText>
-                        </StyledButton>
-                      </Grid>
-                      <Grid>
-                        <StyledButton light color="primary" auto>
-                          <StyledText size={13} weight="medium">
-                            Goal
-                          </StyledText>
-                        </StyledButton>
-                      </Grid>
-                      <Grid>
-                        <StyledButton light color="primary" auto>
-                          <StyledText size={13} weight="medium">
-                            Props
-                          </StyledText>
-                        </StyledButton>
-                      </Grid>
-                    </Grid.Container>
-                  }
-                  css={{ zIndex: 9999 }}
-                >
-                  <StyledButton light color="primary" auto>
-                    <StyledText size={15} weight="medium">
-                      Introduce
-                    </StyledText>
-                  </StyledButton>
-                </Tooltip>
-              </Grid>
-              <Grid>
-                <Tooltip
-                  placement="bottom"
-                  trigger="hover"
-                  hideArrow
-                  content={
-                    <Grid.Container gap={1}>
-                      <Grid>
-                        <StyledButton light color="primary" auto>
-                          <StyledText size={13} weight="medium">
-                            Performance Specialist
-                          </StyledText>
-                        </StyledButton>
-                      </Grid>
-                      <Grid>
-                        <StyledButton light color="primary" auto>
-                          <StyledText size={13} weight="medium">
-                            Professional Course
-                          </StyledText>
-                        </StyledButton>
-                      </Grid>
-                    </Grid.Container>
-                  }
-                  css={{ zIndex: 9999 }}
-                >
-                  <StyledButton light color="primary" auto>
-                    <StyledText size={15} weight="medium">
-                      Program
-                    </StyledText>
-                  </StyledButton>
-                </Tooltip>
-              </Grid>
-              <Grid>
-                <Link href="/instructor">
-                  <StyledButton light color="primary" auto>
-                    <StyledText size={15} weight="medium">
-                      Instructor
-                    </StyledText>
-                  </StyledButton>
-                </Link>
-              </Grid>
-              <Grid>
-                <Link href="/notice">
-                  <StyledButton light color="primary" auto>
-                    <StyledText size={15} weight="medium">
-                      Notice
-                    </StyledText>
-                  </StyledButton>
-                </Link>
-              </Grid>
-              <Grid>
-                <Link href="/faq">
-                  <StyledButton light color="primary" auto>
-                    <StyledText size={15} weight="medium">
-                      FAQ
-                    </StyledText>
-                  </StyledButton>
-                </Link>
-              </Grid>
+              {MenuWithSub(
+                "introduce",
+                SubMenu("introduce", ["history", "goal", "props"])
+              )}
+              {MenuWithSub(
+                "program",
+                SubMenu("program", [
+                  "performance specialist",
+                  "professional course",
+                ])
+              )}
+              {Menu("instructor")}
+              {Menu("notice")}
+              {Menu("faq")}
             </Grid.Container>
           </Grid>
           <Grid xs />
@@ -136,6 +94,24 @@ const StyledHeader = styled.div`
   z-index: 100;
 `;
 
+const MenuBtn = styled.button`
+  background: transparent;
+  padding: 0.6rem 1rem;
+  min-width: min-content;
+  border-radius: 14px;
+  outline: none;
+  border: none;
+  cursor: pointer;
+
+  &::first-letter {
+    text-transform: uppercase;
+  }
+
+  &:hover {
+    background-color: rgba(2, 32, 71, 0.05);
+  }
+`;
+
 const StyledGrid = styled(Grid)`
   backdrop-filter: saturate(180%) blur(10px);
   box-shadow: rgb(2 1 1 / 10%) 0px 5px 20px -5px;
@@ -152,16 +128,13 @@ const StyledContainer = styled(Container)`
 
 const StyledImg = styled.img`
   width: 5rem;
-  height: auto;
 `;
 
 const StyledText = styled(Text)`
   opacity: 0.8;
   font-weight: 500;
-`;
 
-const StyledButton = styled(Button)`
-  &:hover {
-    background-color: rgba(2, 32, 71, 0.05);
+  &::first-letter {
+    text-transform: uppercase;
   }
 `;
