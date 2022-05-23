0c61cf40f75fab51d4ec28cebe7e988414338b68
danbom <bear0369@naver.com>
2022-05-23 22:22:10 +0900

    Feat: "aos 초기화"

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,19 @@ import type { AppProps } from "next/app";
 import Head from "next/head";
 import { NextUIProvider } from "@nextui-org/react";
 import styled from "styled-components";
+import React, { useEffect } from "react";
+import AOS from "aos";
+import "aos/dist/aos.css";
 
 import Header from "../components/Header";
 
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    AOS.init();
+  });
+
   return (
     <>
       <Head>
